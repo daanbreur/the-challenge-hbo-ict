0d7d63cf89e270714e4814f6c59c0a1e5bc579d3
Thijs Kamphuis <thijskamphuis@icloud.com>
2023-11-16 17:12:35 +0100

    bimbimbambam

--- code/ClickerCompanion/main.js
+++ code/ClickerCompanion/main.js
@@ -4,10 +4,14 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 1600,
         height: 900,
-        icon: 'images/icons/icon.png'
+        icon: 'images/icons/icon.png',
+        backgroundMaterial: "mica",
+        autoHideMenuBar: true,
+        resizable: true,
     })
 
-    win.loadFile('index.html')
+    win.loadFile('index.html').then()
+
 }
 app.whenReady().then(() => {
     createWindow()
