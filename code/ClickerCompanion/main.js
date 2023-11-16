2d819458f7629a54ed36fcfcbe6b1c849c2f0647
Thijs Kamphuis <thijskamphuis@icloud.com>
2023-11-16 13:55:54 +0100

    html owo

--- code/ClickerCompanion/main.js
+++ code/ClickerCompanion/main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1600,
+        height: 900,
         icon: 'images/icons/icon.png'
     })
 
