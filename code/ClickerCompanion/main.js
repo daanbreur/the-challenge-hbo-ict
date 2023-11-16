549242aacf47f557cb46ac6c225b91413a246960
Thijs Kamphuis <thijskamphuis@icloud.com>
2023-11-16 12:17:53 +0100

    icons + style

--- code/ClickerCompanion/main.js
+++ code/ClickerCompanion/main.js
@@ -3,7 +3,8 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        icon: 'images/icons/icon.png'
     })
 
     win.loadFile('index.html')
