c8960b418226306b10c02e14543f5248352b8717
Daan Breur <git@daanbreur.systems>
2023-11-19 14:55:05 +0100

    app: remove crappy library
    todo
    - write custom wrapper class

--- code/ClickerCompanion/src/renderer/App.tsx
+++ code/ClickerCompanion/src/renderer/App.tsx
@@ -1,26 +1,28 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+import logo from '../../assets/logo.svg';
+// import './App.css';
 import 'tailwindcss/tailwind.css';
-import { useWebSerial } from '@mniota/react-webserial-hook';
+
+async function requestPort() {
+  const port = await navigator.serial.requestPort();
+  const portInfo = port.getInfo();
+  console.log(port, portInfo);
+}
 
 function Hello() {
-  const serial = useWebSerial({
-    onData: (data) => {
-      const decoder = new TextDecoder();
-      console.log(decoder.decode(data));
-    },
-  });
-
   return (
     <div>
       <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
+        <img width="200" alt="icon" src={logo} />
       </div>
-      <h1>electron-react-boilerplate</h1>
-      <button onClick={() => serial.requestPort()}>Pair a new port</button>
-      <button onClick={() => serial.openPort()}>Open the selected port</button>
-      <button onClick={() => serial.startReading()}>Start reading</button>
+      <h1 className="text-3xl font-bold underline">
+        electron-react-boilerplate
+      </h1>
+      <button type="button" onClick={() => requestPort()}>
+        Pair a new port
+      </button>
+      {/* <button onClick={() => serial.openPort()}>Open the selected port</button>
+      <button onClick={() => serial.startReading()}>Start reading</button> */}
     </div>
   );
 }
