d8936f2a1239214c5240442a5f65f5334112038d
Daan Breur <git@daanbreur.systems>
2023-11-22 10:40:52 +0100

    app: styles moeten we ff aanpassen nog

--- code/ClickerCompanion/tailwind.config.js
+++ code/ClickerCompanion/tailwind.config.js
@@ -4,12 +4,37 @@ module.exports = {
   content: ['./src/renderer/**/*.{js,jsx,ts,tsx,ejs}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        sky: colors.sky,
-        cyan: colors.cyan,
-      },
+    fontSize: {
+      sm: '0.750rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.333rem',
+      '2xl': '1.777rem',
+      '3xl': '2.369rem',
+      '4xl': '3.158rem',
+      '5xl': '4.210rem',
     },
+    fontFamily: {
+      heading: 'Poppins',
+      body: 'Poppins',
+    },
+    fontWeight: {
+      normal: '400',
+      semibold: '600',
+      bold: '700',
+    },
+    colors: {
+      text: 'var(--text)',
+      background: 'var(--background)',
+      primary: 'var(--primary)',
+      secondary: 'var(--secondary)',
+      accent: 'var(--accent)',
+      button1: 'var(--button1)',
+      button2: 'var(--button2)',
+      button3: 'var(--button3)',
+      button4: 'var(--button4)',
+    },
+    extend: {},
   },
   variants: {
     extend: {},
