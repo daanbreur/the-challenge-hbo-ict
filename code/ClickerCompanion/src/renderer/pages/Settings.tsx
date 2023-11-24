5218ef82d06239a852b9111ac26cf0d894cce064
Thijs Kamphuis <thijskamphuis@icloud.com>
2023-11-24 02:25:43 +0100

    cc color + settings

--- code/ClickerCompanion/src/renderer/pages/Settings.tsx
+++ code/ClickerCompanion/src/renderer/pages/Settings.tsx
@@ -10,35 +10,26 @@ import { Props } from '../types';
 export default function Settings({ setCurrentPage }: Props) {
   return (
     <PageWrapper>
-      <div className="flex">
+      <div className="flex justify-between">
         <div
-          onClick={() => {
-            setCurrentPage('mainmenu');
-          }}
-          className="flex-1 text-xl"
-        >
-          &lt; main menu
+          onClick={() => {setCurrentPage('mainmenu');}}
+          className="flex grow-0 shrink-0 px-8 text-xl cursor-pointer bg-cc_blue justify-center items-center rounded-lg m-2 hover:outline">
+          menu
         </div>
-        <div className="text-center text-3xl flex-2">Settings</div>
-        <div className="flex-1"> </div>
+        <div className="text-center text-3xl">Settings</div>
+        <div className="flex grow-0 shrink-0 px-8 text-xl cursor-pointer bg-button3 justify-center items-center rounded-lg m-2 hover:outline">
+          reset
+        </div>
+
       </div>
-      <div className="h-auto">
-        <div className="bg-light_background rounded-lg w-full h-auto mb-5">
-          <p className="text-center mb-3">Quiz</p>
-          <div>Setting 1</div>
-        </div>
-        <div className="bg-light_background rounded-lg w-full h-auto mb-5">
-          <p className="text-center mb-3">Video</p>
-          <div>
-            <input type="range" min="1" max="100" value="50" />
+      <div className="h-auto mx-2">
+        <div className="bg-light_background rounded-lg w-full mb-5 ">
+          <div className="p-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae venenatis risus, id lacinia mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi scelerisque risus tortor, id sagittis eros consequat sed. Duis ac eros sit amet leo ornare consectetur a sit amet justo. Sed egestas enim vel arcu consequat laoreet. Sed venenatis massa eu accumsan volutpat. Donec vel auctor orci, eu ullamcorper diam. Proin quis nulla volutpat, fermentum ligula vel, finibus ante. Etiam ex elit, viverra id cursus nec, vestibulum id urna.
           </div>
         </div>
-        <div className="bg-light_background rounded-lg w-full h-auto">
-          <p className="text-center mb-3">Audio</p>
-          <div>Setting 1</div>
-        </div>
       </div>
-      <footer className="text-center pt-5">
+      <footer className="text-center pt-5 m-auto ">
         ClickerCompanion by ClassClick B.V. 2023
       </footer>
     </PageWrapper>
