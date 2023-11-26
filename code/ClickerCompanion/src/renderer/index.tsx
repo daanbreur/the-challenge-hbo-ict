d9bbc71a56cfad2046754b40169ef9695acdbcb4
Daan Breur <git@daanbreur.systems>
2023-11-26 19:48:05 +0100

    app: beginnetje van die kut database troep

--- code/ClickerCompanion/src/renderer/index.tsx
+++ code/ClickerCompanion/src/renderer/index.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client';
+// eslint-disable-next-line import/no-cycle
 import App from './App';
+import { DatabaseQuery, Question, Quiz } from './types';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
@@ -12,11 +14,42 @@ window.electron.ipcRenderer.once('ipc-example', (arg) => {
 });
 window.electron.ipcRenderer.sendMessage('ipc-example', ['ping']);
 
-const asyncDatabase = async (data: String) => {
+export async function getQuizzes(): Promise<Quiz[]> {
   return new Promise((resolve) => {
-    window.electron.ipcRenderer.once('database-communication', (arg) => {
-      resolve(arg);
-    });
-    window.electron.ipcRenderer.sendMessage('database-communication', data);
+    window.electron.ipcRenderer.once(
+      'database-communication:quizzes',
+      (arg) => {
+        const data: Quiz[] = arg as Quiz[];
+        resolve(data);
+      },
+    );
+    window.electron.ipcRenderer.sendMessage('database-communication', {
+      requestFor: 'quizzes',
+    } as DatabaseQuery);
   });
-};
+}
+
+export async function getQuizQuestions(quizId: Number): Promise<Question[]> {
+  return new Promise((resolve) => {
+    window.electron.ipcRenderer.once(
+      'database-communication:questions',
+      (arg) => {
+        const data: Question[] = arg as Question[];
+        resolve(data);
+      },
+    );
+    window.electron.ipcRenderer.sendMessage('database-communication', {
+      requestFor: 'questions',
+      quizId,
+    } as DatabaseQuery);
+  });
+}
+
+// async function getQuizes(data: String) {
+//   return new Promise((resolve) => {
+//     window.electron.ipcRenderer.once('database-communication', (arg) => {
+//       resolve(arg);
+//     });
+//     window.electron.ipcRenderer.sendMessage('database-communication', data);
+//   });
+// }
