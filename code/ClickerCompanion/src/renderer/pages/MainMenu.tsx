7837da57c9bd116456ee38b48e14d06db052ec66
Thijs Kamphuis <thijskamphuis@icloud.com>
2023-11-26 19:47:13 +0100

    Fix knoppen grid (daan boos)

--- code/ClickerCompanion/src/renderer/pages/MainMenu.tsx
+++ code/ClickerCompanion/src/renderer/pages/MainMenu.tsx
@@ -14,14 +14,19 @@ export default function MainMenu({ setCurrentPage }: Props) {
   return (
     <PageWrapper>
       <div className="flex justify-center items-center py-5">
-        <img draggable={false} className="h-[300px] min-h-[300px]" alt="icon" src={logo} />
+        <img
+          draggable={false}
+          className="h-[300px] min-h-[300px]"
+          alt="icon"
+          src={logo}
+        />
       </div>
-      <div className="grid grid-cols-2 gap-10 font-bold text-3xl">
+      <div className=" h-[50h] grid grid-cols-2 gap-6 font-bold text-3xl">
         <div
           onClick={() => {
             setCurrentPage('quiz-selection');
           }}
-          className=" flex justify-center items-center outline-text hover:outline bg-cc_blue rounded-lg text-center cursor-pointer"
+          className="flex justify-center items-center outline-text hover:outline bg-button1 rounded-lg text-center cursor-pointer py-10"
         >
           Start
         </div>
@@ -29,12 +34,16 @@ export default function MainMenu({ setCurrentPage }: Props) {
           onClick={() => {
             setCurrentPage('settings');
           }}
-          className="flex justify-center items-center outline-text hover:outline bg-button3 rounded-lg text-center cursor-pointer"
+          className="flex justify-center items-center outline-text hover:outline bg-button2 rounded-lg text-center cursor-pointer py-10"
         >
           Settings
         </div>
-        <div className="flex-auto flex justify-center items-center bg-button3 rounded-lg ml-10" />
-        <div className="flex-auto flex justify-center items-center bg-button4 rounded-lg mr-10" />
+        <div className="flex justify-center items-center outline-text hover:outline bg-button3 rounded-lg text-center cursor-pointer py-10">
+          KK KNOP
+        </div>
+        <div className="flex justify-center items-center outline-text hover:outline bg-button4 rounded-lg text-center cursor-pointer py-10">
+          NOG KNOP
+        </div>
       </div>
       <footer className="text-center pt-5">
         ClickerCompanion by ClassClick B.V. 2023
