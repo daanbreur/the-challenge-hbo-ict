3a96f319ea91f38a4fc4c1e1424947dbff2f4f11
Daan Breur <git@daanbreur.systems>
2023-11-27 14:51:41 +0100

    app: kut wrapper

--- code/ClickerCompanion/src/renderer/components/PageWrapper.tsx
+++ code/ClickerCompanion/src/renderer/components/PageWrapper.tsx
@@ -7,7 +7,7 @@ type IProps = {
 export default function PageWrapper({ children }: IProps) {
   return (
     <div className="dark min-h-screen text-text font-body bg-background">
-      <div className="max-w-[540px] min-h-screen m-auto">{children}</div>
+      {children}
     </div>
   );
 }
