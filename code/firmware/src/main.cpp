2927ea50080a0ef22a1d1c79fcfc6f8c405b5f60
Daan Breur <git@daanbreur.systems>
2023-12-06 13:22:15 +0100

    ait wallah

--- code/firmware/src/main.cpp
+++ code/firmware/src/main.cpp
@@ -1,166 +1,187 @@
-#include "Arduino.h"
-#include "Pins.h"
-#include "Globals.h"
-#include "Bounce2.h"
-#include "ledFunctions.h"
-
-#include <espnow.h>
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <espnow.h>
+#include <ArduinoJson.h>
+#include <EEPROM.h>
 
-uint8_t broadcastAddress[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-typedef struct struct_message {
-  char a[32];
-  int b;
-  float c;
-  String d;
-  bool e;
+struct
+{
+  int deviceId = 1;
+} settings;
+
+int chan;
+
+enum MessageType
+{
+  PAIRING,
+  DATA,
+};
+MessageType messageType;
+
+int counter = 0;
+
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message
+{
+  uint8_t msgType;
+  uint8_t id;
+  float temp;
+  float hum;
+  unsigned int readingId;
 } struct_message;
-struct_message myData;
 
-void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(sendStatus == 1 ? "Delivery Success" : "Delivery Fail");
+typedef struct struct_pairing
+{ // new structure for pairing
+  uint8_t msgType;
+  uint8_t id;
+  uint8_t macAddr[6];
+  uint8_t channel;
+} struct_pairing;
+
+struct_message incomingReadings;
+struct_message outgoingSetpoints;
+struct_pairing pairingData;
+
+void readDataToSend()
+{
+  outgoingSetpoints.msgType = DATA;
+  outgoingSetpoints.id = 0;
+  outgoingSetpoints.temp = random(0, 40);
+  outgoingSetpoints.hum = random(0, 100);
+  outgoingSetpoints.readingId = counter++;
 }
 
-Bounce2::Button buttons[4] = {Bounce2::Button(), Bounce2::Button(), Bounce2::Button(), Bounce2::Button()};
-
-long lastMicros = 0;
-long lastStatusUpdate = 0;
-bool setRandomColor = true;
-
-bool usbPowerConnected() {
-  return digitalRead(CHARGE) == LOW || digitalRead(STANDBY) == LOW;
-}
-bool isCharging() {
-  return digitalRead(CHARGE) == LOW;
-}
-float batteryVoltage() {
-  float voltage = analogRead(BATTERY);
-  return (voltage * 0.00416918) + 0.0;
-}
-bool powerSwitchOn() {
-  return batteryVoltage() < 1;
+// ---------------------------- esp_ now -------------------------
+void printMAC(const uint8_t *mac_addr)
+{
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
 }
 
-
-void updateButtons() {
-  for(int i = 0; i < 4; i++) {
-    buttons[i].update();
+bool addPeer(uint8_t *peer_addr)
+{ // add pairing
+  // check if the peer exists
+  if (esp_now_is_peer_exist(peer_addr))
+  {
+    // Slave already paired.
+    Serial.println("Already Paired");
+    return true;
+  }
+  else
+  {
+    if (esp_now_add_peer(peer_addr, ESP_NOW_ROLE_COMBO, chan, NULL, 0))
+    {
+      // Pair success
+      Serial.println("Pair success");
+      return true;
+    }
+    else
+    {
+      Serial.println("Pair failed");
+      return false;
+    }
   }
 }
 
-/**
- * Calculates the time elapsed in microseconds since the last run of the function.
- * 
- * @return The time elapsed in microseconds.
- */
-int microsSinceLastRun() {
-  long currentNano = micros();
-  int diff = (currentNano - lastMicros + 4294967295) % 4294967296;
-  lastMicros = currentNano;
-  return diff;
+// callback when data is sent
+void OnDataSent(uint8_t *mac_addr, uint8_t status)
+{
+  Serial.print("Last Packet Send Status: ");
+  Serial.print(status == 1 ? "Delivery Success to " : "Delivery Fail to ");
+  printMAC(mac_addr);
+  Serial.println();
 }
 
-void setup(){
-  Serial.begin(115200);
+// void OnDataRecv(uint8_t *mac_addr, const uint8_t *incomingData, uint8_t len)
+void OnDataRecv(uint8_t *mac_addr, uint8_t *incomingData, uint8_t len)
+{
+  Serial.print(len);
+  Serial.print(" bytes of data received from : ");
+  printMAC(mac_addr);
+  Serial.println();
+  StaticJsonDocument<1000> root;
+  String payload;
+  uint8_t type = incomingData[0]; // first message byte is the type of message
+  switch (type)
+  {
+  case DATA: // the message is data type
+    memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+    // create a JSON document with received data and send it by event to the web page
+    root["id"] = incomingReadings.id;
+    root["temperature"] = incomingReadings.temp;
+    root["humidity"] = incomingReadings.hum;
+    root["readingId"] = String(incomingReadings.readingId);
+    // serializeJson(root, payload);
+    Serial.print("event send :");
+    serializeJson(root, Serial);
+    // events.send(payload.c_str(), "new_readings", millis());
+    Serial.println();
+    break;
 
-  setupLeds();
-
-  pinMode(CHARGE, INPUT);
-  pinMode(STANDBY, INPUT);
-  pinMode(BATTERY, INPUT);
-
-  for(int i = 0; i < 4; i++) {
-    if(i == 0) buttons[i].attach(BUTTON_1, INPUT);
-    if(i == 1) buttons[i].attach(BUTTON_2, INPUT);
-    if(i == 2) buttons[i].attach(BUTTON_3, INPUT);
-    if(i == 3) buttons[i].attach(BUTTON_4, INPUT);
-
-    buttons[i].interval(25);
-    buttons[i].setPressedState(HIGH);
+  case PAIRING: // the message is a pairing request
+    memcpy(&pairingData, incomingData, sizeof(pairingData));
+    Serial.println(pairingData.msgType);
+    Serial.println(pairingData.id);
+    Serial.print("Pairing request from: ");
+    printMAC(mac_addr);
+    Serial.println();
+    Serial.println(pairingData.channel);
+    if (pairingData.id > 0)
+    { // do not replay to server itself
+      if (pairingData.msgType == PAIRING)
+      {
+        pairingData.id = 0;
+        pairingData.channel = chan;
+        Serial.println("send response");
+        esp_now_send(mac_addr, (uint8_t *)&pairingData, sizeof(pairingData));
+        addPeer(mac_addr);
+      }
+    }
+    break;
   }
+}
 
-  for(int i = 0; i < 5; i++) {
-    addBlockTransitionToStack(i, CRGB::Red, 25, i * 5, 5);
-    addBlockTransitionToStack(i, CRGB::Orange, 25, 0, 5);
-    addBlockTransitionToStack(i, CRGB::Yellow, 25, 0, 5);
-    addBlockTransitionToStack(i, CRGB::Green, 25, 0, 5);
-    addBlockTransitionToStack(i, CRGB::Blue, 25, 0, 5);
-    addBlockTransitionToStack(i, CRGB::Indigo, 25, 0, 5);
-    addBlockTransitionToStack(i, CRGB::Black, 25, 0, 5);
-  }
-
-  // Serial.println(getTransitionStackLength());
-  // Serial.println("heyy");
-  // Serial.println(getTransitionStackLength());
-  Serial.println("Device MAC: " + WiFi.macAddress());
-
-  WiFi.mode(WIFI_STA);
-
-  if (esp_now_init() != 0) {
+void initESP_NOW()
+{
+  if (esp_now_init() != 0)
+  {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-
   esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
   esp_now_register_send_cb(OnDataSent);
-
-  if (esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0)){ 
-    Serial.println("Failed to add peer");
-    return;
-  } else {
-    Serial.println("Added peer?");
-  }
+  esp_now_register_recv_cb(OnDataRecv);
 }
 
-void loop(){
-  updateButtons();
+void setup()
+{
+  // Initialize Serial Monitor
+  Serial.begin(115200);
 
-  for(int i = 0; i < 4; i++) {
-    if(buttons[i].pressed()) {
-      if(!setRandomColor)setBlockColor(i + 1, CRGB::White);
-      if(setRandomColor) setBlockColor(i + 1, randomColor());
-    } else if (buttons[i].released()) {
-      setBlockColor(i + 1, CRGB::Black, 100, 0, 0);
-    }
+  EEPROM.begin(sizeof(settings));
+  EEPROM.get(0, settings);
+
+  Serial.println();
+  WiFi.mode(WIFI_STA);
+  chan = 10; // TODO: Bedenk ff iets logisch voor channel scanning.
+
+  Serial.print("MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+
+  initESP_NOW();
+}
+
+void loop()
+{
+  static unsigned long lastEventTime = millis();
+  static const unsigned long EVENT_INTERVAL_MS = 5000;
+  if ((millis() - lastEventTime) > EVENT_INTERVAL_MS)
+  {
+    lastEventTime = millis();
+    readDataToSend();
+    esp_now_send(NULL, (uint8_t *)&outgoingSetpoints, sizeof(outgoingSetpoints));
   }
-
-  if(usbPowerConnected() == true && isCharging() == false) {
-    setBlockColor(0, CRGB::Green);
-  }
-
-  if(millis() - lastStatusUpdate > 1000) {
-    Serial.print("Battery: ");
-    Serial.print(batteryVoltage());
-    Serial.print(" | Charge: ");
-    Serial.print(isCharging());
-    Serial.print(" | 1: ");
-        Serial.print(digitalRead(CHANGE));
-    Serial.print(" | 2: ");
-        Serial.print(digitalRead(STANDBY));
-    Serial.print(" | USB: ");
-    Serial.println(usbPowerConnected());
-
-    if(usbPowerConnected() == true && isCharging() == false) {
-      Serial.println("USB Power Connected");
-      addLedTransitionToStack(0, CRGB(0, 10, 0), 100, 0);
-      addLedTransitionToStack(0, CRGB::Black, 100, 0);
-    } else if (usbPowerConnected() == true && isCharging() == true) {
-      Serial.println("Charging");
-      addLedTransitionToStack(0, CRGB(10, 0, 0), 100, 0);
-      addLedTransitionToStack(0, CRGB::Black, 100, 0);
-    } else if (usbPowerConnected() == false) {
-      Serial.println("USB Power Disconnected");
-      setLedColor(0, CRGB(0, 10, 0), 100, 0);
-    }
-
-    lastStatusUpdate = millis();
-  }
-
-  handleTransitionStack();
-  handleLedFadeStep();
-  FastLED.show();
-
-  int microsSinceLast = microsSinceLastRun();
-  if(microsSinceLast < 1000) delayMicroseconds(1000 - microsSinceLast);
 }
