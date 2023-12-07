ea16cfbe894e28dc31f4d35cf2f102e67adbd35b
Jeroen de Kuijper <jdk@dekuijperict.nl>
2023-12-07 09:47:47 +0100

    ESPNOW

--- code/firmware/src/main.cpp
+++ code/firmware/src/main.cpp
@@ -3,14 +3,41 @@
 #include "Bounce2.h"
 #include "ledFunctions.h"
 
-#include <espnow.h>
 #include <ESP8266WiFi.h>
+#define WIFI_MODE_STA WIFI_STA 
+#include <QuickEspNow.h>
+
+enum EspNowMessageType {
+  STATUS_UPDATE, 
+  BUTTON_STATE,
+  TIME_SYNC
+};
+
+struct EspNowMessage{
+  EspNowMessageType messageType;
+  int deviceId;
+  long timestamp = 0;
+  float batteryVolt = -1;
+  bool isCharging = false;
+  bool usbPowerConnected = false;
+  bool button1Pressed = false;
+  bool button2Pressed = false;
+  bool button3Pressed = false;
+  bool button4Pressed = false;
+};
+
 
 Bounce2::Button buttons[4] = {Bounce2::Button(), Bounce2::Button(), Bounce2::Button(), Bounce2::Button()};
 
 long lastMicros = 0;
 long lastStatusUpdate = 0;
 bool setRandomColor = true;
+bool cancer = true;
+
+
+long timeSync = 0;
+long timeSyncLocal = 0; 
+int timeSyncDeviation = 0;
 
 bool usbPowerConnected() {
   return digitalRead(CHARGE) == LOW || digitalRead(STANDBY) == LOW;
@@ -45,6 +72,38 @@ int microsSinceLastRun() {
   return diff;
 }
 
+void dataSent (uint8_t* address, uint8_t status) {
+    Serial.printf ("Message sent to " MACSTR ", status: %d\n", MAC2STR (address), status);
+    delay(1);
+}
+
+void dataReceived (uint8_t* address, uint8_t* data, uint8_t len, signed int rssi, bool broadcast) {
+    Serial.print ("Received: ");
+    Serial.printf ("%.*s\n", len, data);
+    Serial.printf ("RSSI: %d dBm\n", rssi);
+    Serial.printf ("From: " MACSTR "\n", MAC2STR (address));
+    Serial.printf ("%s\n", broadcast ? "Broadcast" : "Unicast");
+
+    String message = String((char*)data);
+    long timestamp = message.toInt();
+
+    if(timestamp > 0) {
+      long millisPassed = millis() - timeSyncLocal;
+      long expectedMillisPassed = timeSync + millisPassed;
+      long actualMillis = timestamp;
+
+      Serial.println("Millis Passed: " + String(millisPassed));
+      Serial.println("Expected Millis Passed: " + String(expectedMillisPassed));
+      Serial.println("Actual Millis: " + String(actualMillis));
+
+
+
+      timeSyncLocal = millis();
+      timeSync = timestamp;
+    }
+}
+
+
 void setup(){
   Serial.begin(115200);
 
@@ -64,6 +123,12 @@ void setup(){
     buttons[i].setPressedState(HIGH);
   }
 
+  WiFi.mode (WIFI_MODE_STA);
+  WiFi.disconnect (false);
+  quickEspNow.onDataSent(dataSent);
+  quickEspNow.onDataRcvd(dataReceived);
+  quickEspNow.begin (1);
+
   for(int i = 0; i < 5; i++) {
     addBlockTransitionToStack(i, CRGB::Red, 25, i * 5, 5);
     addBlockTransitionToStack(i, CRGB::Orange, 25, 0, 5);
@@ -80,10 +145,8 @@ void setup(){
     Serial.println("Device MAC: " + WiFi.macAddress());
 
 }
-
 void loop(){
   updateButtons();
-
   for(int i = 0; i < 4; i++) {
     if(buttons[i].pressed()) {
       if(!setRandomColor)setBlockColor(i + 1, CRGB::White);
@@ -121,6 +184,16 @@ void loop(){
       Serial.println("USB Power Disconnected");
       setLedColor(0, CRGB(0, 10, 0), 100, 0);
     }
+
+
+    // EspNowMessage statusMessage = EspNowMessage();
+    // statusMessage.messageType = EspNowMessageType::STATUS_UPDATE;
+    // statusMessage.deviceId = 1;
+    // statusMessage.batteryVolt = batteryVoltage();
+    // statusMessage.isCharging = isCharging();
+    // statusMessage.usbPowerConnected = usbPowerConnected();
+    // String message = String(statusMessage.messageType) + "," + String(statusMessage.deviceId) + "," + String(statusMessage.batteryVolt) + "," + String(statusMessage.isCharging) + "," + String(statusMessage.usbPowerConnected);
+    // quickEspNow.send (ESPNOW_BROADCAST_ADDRESS , (uint8_t*)message.c_str (), message.length ());
 
     lastStatusUpdate = millis();
   }
