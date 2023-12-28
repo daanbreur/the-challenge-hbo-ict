c73bcbf750422b5aa5a661d3668bd14aa3680999
Daan Breur <git@daanbreur.systems>
2023-12-28 22:42:21 +0100

    hub firmware

--- code/firmware/src/main.cpp
+++ code/firmware/src/main.cpp
@@ -3,11 +3,7 @@
 #include <espnow.h>
 #include <ArduinoJson.h>
 #include <EEPROM.h>
-
-struct
-{
-  int deviceId = 1;
-} settings;
+#include <Debug.h>
 
 int chan;
 
@@ -53,34 +49,45 @@ void readDataToSend()
 }
 
 // ---------------------------- esp_ now -------------------------
+void getMAC(char *buf, const uint8_t *mac_addr)
+{
+  sprintf(buf, "%02x:%02x:%02x:%02x:%02x:%02x",
+          mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+}
+
 void printMAC(const uint8_t *mac_addr)
 {
   char macStr[18];
   snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  Serial.print(macStr);
+  D_print(macStr);
+}
+
+void readMAC(const char *buf, uint8_t *mac_addr)
+{
+  sscanf(buf, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+         &mac_addr[0], &mac_addr[1], &mac_addr[2], &mac_addr[3], &mac_addr[4], &mac_addr[5]);
 }
 
 bool addPeer(uint8_t *peer_addr)
-{ // add pairing
-  // check if the peer exists
+{
   if (esp_now_is_peer_exist(peer_addr))
   {
-    // Slave already paired.
-    Serial.println("Already Paired");
+    D_println("Already Paired");
     return true;
   }
   else
   {
-    if (esp_now_add_peer(peer_addr, ESP_NOW_ROLE_COMBO, chan, NULL, 0))
+    if (esp_now_add_peer(peer_addr, ESP_NOW_ROLE_COMBO, chan, NULL, 0) == 0)
     {
-      // Pair success
-      Serial.println("Pair success");
+      D_println("Pair success");
       return true;
     }
     else
     {
-      Serial.println("Pair failed");
+      D_print("Failed to add peer: ");
+      printMAC(peer_addr);
+      D_println("");
       return false;
     }
   }
@@ -89,19 +96,18 @@ bool addPeer(uint8_t *peer_addr)
 // callback when data is sent
 void OnDataSent(uint8_t *mac_addr, uint8_t status)
 {
-  Serial.print("Last Packet Send Status: ");
-  Serial.print(status == 1 ? "Delivery Success to " : "Delivery Fail to ");
+  D_print("\r\nLast Packet Send Status: ");
+  D_print(status == 0 ? "Delivery Success to " : "Delivery Fail to ");
   printMAC(mac_addr);
-  Serial.println();
+  D_println();
 }
 
 // void OnDataRecv(uint8_t *mac_addr, const uint8_t *incomingData, uint8_t len)
 void OnDataRecv(uint8_t *mac_addr, uint8_t *incomingData, uint8_t len)
 {
-  Serial.print(len);
-  Serial.print(" bytes of data received from : ");
+  D_printf("\r\n --- \r\n%d bytes of data received from: ", len);
   printMAC(mac_addr);
-  Serial.println();
+  D_println();
   StaticJsonDocument<1000> root;
   String payload;
   uint8_t type = incomingData[0]; // first message byte is the type of message
@@ -118,26 +124,28 @@ void OnDataRecv(uint8_t *mac_addr, uint8_t *incomingData, uint8_t len)
     Serial.print("event send :");
     serializeJson(root, Serial);
     // events.send(payload.c_str(), "new_readings", millis());
-    Serial.println();
+    D_println();
     break;
 
   case PAIRING: // the message is a pairing request
     memcpy(&pairingData, incomingData, sizeof(pairingData));
-    Serial.println(pairingData.msgType);
-    Serial.println(pairingData.id);
-    Serial.print("Pairing request from: ");
+    D_printf("msgtype: %d | id: %d | channel: %d\r\n", pairingData.msgType, pairingData.id, pairingData.channel);
+    D_print("Pairing request from: ");
     printMAC(mac_addr);
-    Serial.println();
-    Serial.println(pairingData.channel);
+    D_println();
     if (pairingData.id > 0)
     { // do not replay to server itself
       if (pairingData.msgType == PAIRING)
       {
-        pairingData.id = 0;
-        pairingData.channel = chan;
-        Serial.println("send response");
-        esp_now_send(mac_addr, (uint8_t *)&pairingData, sizeof(pairingData));
-        addPeer(mac_addr);
+        char macStr[18];
+        getMAC(macStr, mac_addr);
+
+        StaticJsonDocument<128> doc;
+        doc["type"] = "pairing";
+        doc["data"]["id"] = pairingData.id;
+        doc["data"]["macaddr"] = macStr;
+        serializeJson(doc, Serial);
+        D_println();
       }
     }
     break;
@@ -148,7 +156,7 @@ void initESP_NOW()
 {
   if (esp_now_init() != 0)
   {
-    Serial.println("Error initializing ESP-NOW");
+    D_println("Error initializing ESP-NOW");
     return;
   }
   esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
@@ -158,30 +166,58 @@ void initESP_NOW()
 
 void setup()
 {
-  // Initialize Serial Monitor
   Serial.begin(115200);
 
-  EEPROM.begin(sizeof(settings));
-  EEPROM.get(0, settings);
-
-  Serial.println();
+  D_println();
   WiFi.mode(WIFI_STA);
   chan = 10; // TODO: Bedenk ff iets logisch voor channel scanning.
 
-  Serial.print("MAC Address:  ");
-  Serial.println(WiFi.macAddress());
+  D_print("MAC Address:  ");
+  D_println(WiFi.macAddress());
 
   initESP_NOW();
 }
 
 void loop()
 {
-  static unsigned long lastEventTime = millis();
-  static const unsigned long EVENT_INTERVAL_MS = 5000;
-  if ((millis() - lastEventTime) > EVENT_INTERVAL_MS)
+  // static unsigned long lastEventTime = millis();
+  // static const unsigned long EVENT_INTERVAL_MS = 5000;
+  // if ((millis() - lastEventTime) > EVENT_INTERVAL_MS)
+  // {
+  //   lastEventTime = millis();
+  //   readDataToSend();
+  //   esp_now_send(NULL, (uint8_t *)&outgoingSetpoints, sizeof(outgoingSetpoints));
+  // }
+
+  if (Serial.available())
   {
-    lastEventTime = millis();
-    readDataToSend();
-    esp_now_send(NULL, (uint8_t *)&outgoingSetpoints, sizeof(outgoingSetpoints));
+    StaticJsonDocument<128> doc;
+    DeserializationError err = deserializeJson(doc, Serial);
+
+    if (err == DeserializationError::Ok)
+    {
+      if (doc["type"] == "accept_pairing")
+      {
+        const char *macStr = doc["data"]["macaddr"];
+        uint8_t mac_addr[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+        readMAC(macStr, mac_addr);
+
+        struct_pairing pairData;
+        pairData.msgType = PAIRING;
+        pairData.id = 0;
+        pairData.channel = chan;
+        WiFi.softAPmacAddress(pairData.macAddr);
+
+        esp_now_send(mac_addr, (uint8_t *)&pairData, sizeof(pairData));
+        addPeer(mac_addr);
+      }
+    }
+    else
+    {
+      D_print("deserializeJson() returned ");
+      D_println(err.c_str());
+      while (Serial.available() > 0)
+        Serial.read();
+    }
   }
 }
