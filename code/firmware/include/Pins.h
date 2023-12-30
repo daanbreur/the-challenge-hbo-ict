0f7e1a7ca37d925c0213fc4a930b8c4659849158
Daan Breur <git@daanbreur.systems>
2023-12-30 23:38:34 +0100

    library stuffies

--- code/firmware/include/Pins.h
+++ code/firmware/include/Pins.h
@@ -2,7 +2,7 @@
 #define BUTTON_2 13
 #define BUTTON_3 15
 #define BUTTON_4 5
-#define LEDS_DATA   4
-#define CHARGE	 14
-#define STANDBY  16
+#define LEDS_DATA 4
+#define CHARGE 14
+#define STANDBY 16
 #define BATTERY 17
