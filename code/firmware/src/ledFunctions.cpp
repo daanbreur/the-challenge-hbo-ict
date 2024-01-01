bc76d2d6e81419f4ac41abfe133d653facd5d43b
Daan Breur <git@daanbreur.systems>
2024-01-01 18:40:03 +0100

    mijn ogen doen pijn :sob:

--- code/firmware/src/ledFunctions.cpp
+++ code/firmware/src/ledFunctions.cpp
@@ -115,7 +115,7 @@ bool ledHasTransition(int ledIndex) {
  */
 void setupLeds(){
   FastLED.addLeds<WS2812B, LEDS_DATA, RGB>(leds_buffer, NUM_LEDS);
-  FastLED.setBrightness(255);
+  FastLED.setBrightness(128);
 }
 
 class ledTransition { 
